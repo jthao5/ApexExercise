bb697e9e15815d15110b21f0bb06d01476115235
jthao <jthao5@hotmail.com>
2018-03-11 23:58:25 -0500

    Full Commit 1

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Apex Project Example (Johnny Thao)</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Apex Project Example (Johnny Thao)</h1>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Please Select Begin Date" Font-Names="Calibri" Font-Underline="True"></asp:Label>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" Font-Names="Calibri" TitleStyle-BackColor="#990000" TitleStyle-ForeColor="White"></asp:Calendar>
+            <asp:Label ID="Label3" runat="server" Text="Please Select End Date" Font-Names="Calibri" Font-Underline="True"></asp:Label>
+            <asp:Calendar ID="Calendar2" runat="server" OnSelectionChanged="Calendar2_SelectionChanged" Font-Names="Calibri" TitleStyle-BackColor="#990000" TitleStyle-ForeColor="White"></asp:Calendar>
+            <asp:Label ID="Label5" runat="server" Text="Label" Font-Names="Calibri"></asp:Label><br />
+            <asp:Button ID="Button1" runat="server" Text="View the First 15 Records" OnClick="Button1_OnClick" Font-Names="Calibri" />
+            <asp:Button ID="Button2" runat="server" Text="Export All Records to Excel" OnClick="Button2_OnClick" Font-Names="Calibri" /><br />
+            <asp:Label ID="Label6" runat="server" Text="No Records Found" Visible="False" Font-Names="Calibri" Font-Bold="True" Font-Size="Larger"></asp:Label>
+            <asp:GridView ID="GridView1"  runat="server" AutoGenerateColumns="False" Font-Names="Calibri">
+                <Columns>
+                    <asp:BoundField DataField="Sold_At" HeaderText="Sold At">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="Sold_To" HeaderText="Sold To">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="Account_Number" HeaderText="Account Number">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="Invoice__" HeaderText="Invoice #">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="Customer_PO__" HeaderText="Customer PO #">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="OrderDateText" HeaderText="Order Date">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="DueDateText" HeaderText="Due Date">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="Invoice_Total" HeaderText="Invoice Total" htmlencode="false" DataFormatString = "{0:C2}">
+                    <ControlStyle Width="50px" />
+                    </asp:BoundField>
+                </Columns>
+            </asp:GridView>  
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.IO;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            //Sets the default begin date (the first date of last month) and the end date (the last date of the last month) on the initial page load
+            DateTime lM1Date;
+            lM1Date = new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, 1);
+
+            Calendar1.SelectedDate = lM1Date;
+            Calendar1.VisibleDate = DateTime.Now.AddMonths(-1);
+
+            DateTime llMDate;
+            llMDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month - 1));
+
+            Calendar2.SelectedDate = llMDate;
+            Calendar2.VisibleDate = DateTime.Now.AddMonths(-1);
+            Label5.Text = "Range is: " + Calendar1.SelectedDate.ToShortDateString() + " to " + Calendar2.SelectedDate.ToShortDateString();
+        }
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        //Notifies user of date range being changed
+        Label5.Text = "Range is: " + Calendar1.SelectedDate.ToShortDateString() + " to " + Calendar2.SelectedDate.ToShortDateString();
+    }
+
+    protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+    {
+        //Notifies user of date range being changed
+        Label5.Text = "Range is: " + Calendar1.SelectedDate.ToShortDateString() + " to " + Calendar2.SelectedDate.ToShortDateString();
+    }
+
+    protected void Button1_OnClick(object sender, EventArgs e)
+    {
+        //Uses DataClassesDataContext from LinqToSql to obtain connection to data from the database
+        DataClassesDataContext dtContext = new DataClassesDataContext();
+
+        var beginDate = Convert.ToDateTime(Calendar1.SelectedDate.ToShortDateString());
+        var endDate = Convert.ToDateTime(Calendar2.SelectedDate.ToShortDateString());
+
+        //LinqToSql query to grab data from the first 15 records
+        var query = (from viewApex in dtContext.viewApexes
+                     where viewApex.Order_Date >= beginDate && viewApex.Due_Date <= endDate
+                     orderby viewApex.Account_Number
+                     select new { viewApex.Sold_At, viewApex.Sold_To, viewApex.Account_Number, viewApex.Invoice__, viewApex.Customer_PO__, viewApex.OrderDateText, viewApex.DueDateText, viewApex.Invoice_Total}).Take(15);
+
+        //Gridview to show the user the first 15 records
+        GridView1.DataSource = query;
+        GridView1.DataBind();
+
+        //Label alerting if no records were found
+        if (query.Count() == 0)
+            Label6.Visible = true;
+        else
+            Label6.Visible = false;
+    }
+
+protected void Button2_OnClick(object sender, EventArgs e)
+    {
+        //Uses DataClassesDataContext from LinqToSql to obtain connection to data from the database
+        DataClassesDataContext dtContext = new DataClassesDataContext();
+
+        var beginDate = Convert.ToDateTime(Calendar1.SelectedDate.ToShortDateString());
+        var endDate = Convert.ToDateTime(Calendar2.SelectedDate.ToShortDateString());
+
+        //LinqToSql query to grab data for all available records
+        var query = (from viewApex in dtContext.viewApexes
+                         where viewApex.Order_Date >= beginDate && viewApex.Due_Date <= endDate
+                     orderby viewApex.Account_Number
+                     select new { viewApex.Sold_At, viewApex.Sold_To, viewApex.Account_Number, viewApex.Invoice__, viewApex.Customer_PO__, viewApex.OrderDateText, viewApex.DueDateText, viewApex.Invoice_Total });
+
+        //Creates a list to obtain the records from the LinqToSql query
+        List<List<string>> data = new List<List<string>>();
+        foreach (dynamic item in query)
+        {
+            Console.WriteLine(item);
+            var Sold_At = (item.Sold_At);
+            var Sold_To= (item.Sold_To);
+            var Account_Number = (item.Account_Number);
+            var Invoice__= (item.Invoice__);
+            var Customer_PO__ = (item.Customer_PO__);
+            var Order_Date = (item.OrderDateText);
+            var Due_Date = (item.DueDateText);
+            var Invoice_Total = (item.Invoice_Total);
+
+            data.Add(new List<String> { Sold_At, Sold_To, Account_Number, Invoice__, Customer_PO__, Convert.ToString(Order_Date), Convert.ToString(Due_Date), Convert.ToString(Invoice_Total) });
+        }
+        query.AsEnumerable();
+        
+        //Creation of the Excel Package
+        ExcelPackage excel = new ExcelPackage();
+        var workSheet = excel.Workbook.Worksheets.Add("Output");
+
+        //Defines the column names for the Excel output
+        IEnumerable<String> columnnames = new string[] { "Sold At", "Sold To", "Account Number", "Invoice #", "Customer PO #", "Order Date", "Due Date", "Invoice Total" };
+
+        //Utilizes methods from the prvoided SpreadsheetBuilderWithExample.linq (pasted the code into the Class2.cs file)
+        SpreadsheetBuilder.SaveData(workSheet, columnnames, data);
+
+        //Process to save the file into an Excel file
+        Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        Response.AddHeader("content-disposition", "attachment;  filename=Excel.xlsx");
+        MemoryStream stream = new MemoryStream(excel.GetAsByteArray());
+
+        Response.OutputStream.Write(stream.ToArray(), 0, stream.ToArray().Length);
+        Response.Flush();
+        Response.End();
+        Response.Close();
+    }
+}
